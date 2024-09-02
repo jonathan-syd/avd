b472d08a7b8c3932011eeea05a7b6bfdb836961a
jonathan-syd <164477533+jonathan-syd@users.noreply.github.com>
2024-09-02 15:35:19 +0200

    Update language_fr.ps1

--- language_fr.ps1
+++ language_fr.ps1
@@ -1,10 +1,14 @@
-if (Get-Language -Language en-US){
-    Install-Language -Language fr-FR
-    Set-SystemPreferredUILanguage -Language fr-FR
-    Exit
-    }
-     else  
-    {       
-        Set-SystemPreferredUILanguage -Language fr-FR
-        Exit   
-    }
+$LanguageFr = "fr-Fr"
+$LanguageUS = "en-US"
+
+
+if (Get-Language -Language $LanguageUS){
+    Install-Language -Language $LanguageFr
+}
+
+Set-PreferredLanguage -Language $LanguageFr
+Set-SystemLanguage -Language $LanguageFr
+Set-SystemPreferredUILanguage -Language $LanguageFr
+Set-WinSystemLocale -SystemLocale $LanguageFr
+Set-WinHomeLocation -GeoID 84
+Set-Culture $LanguageFr
